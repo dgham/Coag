3ce81349f3722e27917cfc7d31eba84c5e172b78
AmiraDg <amiradgham15@gmail.com>
2020-08-26 10:30:10 +0100

    new

--- src/Controller/WebPagesController.php
+++ src/Controller/WebPagesController.php
@@ -52,43 +52,8 @@ class WebPagesController extends AbstractController
     }
     else{
         return $this -> render('web_pages/resetPassword.html.twig');
-    }
-
-
-
-
-
-
-
-        
-     
+    } 
     
     }
-    /**
-     * @Rest\Get("/ResetResponse", name ="Confirmation_resetting")
-     */
-    public function ConfirmReset( Request $request)
-    {
-
-        $password = $request->query->get('password');
-        $confirm_password = $request->query->get('confirm_Password');
-        $token = $this->container->getParameter('token');
-        $repository = $this->getDoctrine()->getRepository(User::class);
-        $user = $repository->findOneBy(array('confirmationToken' => $token));
-
-        if (!is_null($user)) {
-            $hash = $encoder->encodePassword($user, $password);
-            $user->setPassword($hash);
-            $user->setConfirmationToken(base_convert(sha1(uniqid(mt_rand(), true)), 16, 36));
-            $em = $this->getDoctrine()->getManager();
-            $em->flush();
-            $this->addFlash('success',' Your password has been reset!');
-             return $this->redirectToRoute('reset_pages');
-        } else {
-
-           $this->addFlash('error',' sorry!, your session expired');
-           return $this->redirectToRoute('reset_pages');
-        }
-
-    }
+   
 }
