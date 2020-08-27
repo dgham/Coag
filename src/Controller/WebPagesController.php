aeb22757c02781a4c3e99526c21f402a94146143
AmiraDg <amiradgham15@gmail.com>
2020-08-27 21:55:22 +0100

    new

--- src/Controller/WebPagesController.php
+++ src/Controller/WebPagesController.php
@@ -37,13 +37,55 @@ class WebPagesController extends AbstractController
     {
         $session = new Session(new PhpBridgeSessionStorage());
         $session->start();
-                   $token= $request->query->get('token');
+        if ($request->getMethod() === 'POST'){
+        $token= $request->query->get('token');
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $user = $repository->findOneBy(array('confirmationToken' => $token));  
+        if (!is_null($user)) {
+        $password = $request->get('password');
+        $hash = $encoder->encodePassword($user,$password);
+        $user->setPassword($hash);
+        $user->setUpdatedAt(new \DateTime());
+        $user->setConfirmationToken(base_convert(sha1(uniqid(mt_rand(), true)), 16, 36));
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($user);
+        $em->flush();
+        $token= $request->query->get('token');
+        $this->addFlash('success', 'your password updated!'); 
+        return $this->render('web_pages/resetPassword.html.twig', [
+            'token' =>  $token,
+        ]);
+       
+        }
+         else{
+            $token= $request->query->get('token');
+            $this->addFlash('danger', 'sorry! your session expired ');
+            return $this->render('web_pages/resetPassword.html.twig', [
+                'token' =>  $token,
+            ]);
+            }
+            }
+            else{
+
+                $token= $request->query->get('token');
+                $repository = $this->getDoctrine()->getRepository(User::class);
+                $user = $repository->findOneBy(array('confirmationToken' => $token));  
+                if ($user === null) {
+                $this->addFlash('danger', 'sorry! your session expired ');
                 return $this->render('web_pages/resetPassword.html.twig', [
                     'token' =>  $token,
                 ]);
+                
+            }
+            else{
+                return $this->render('web_pages/resetPassword.html.twig', [
+                    'token' =>  $token,
+                ]); 
+            }
+        
                 }
 
-
+            }
 
 
 
