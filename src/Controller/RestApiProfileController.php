706c8d24fbc59049811bba367b3a64add9e111b1
Amira Dgham <amira.dgham@continuousnet.com>
2020-11-11 09:10:48 +0100

    add try catch

--- src/Controller/RestApiProfileController.php
+++ src/Controller/RestApiProfileController.php
@@ -2,27 +2,28 @@
 
 namespace App\Controller;
 
-use App\Entity\Country;
-use App\Entity\Doctor;
-use App\Entity\Hospital;
-use App\Entity\Patient;
-use App\Entity\Speciality;
-use App\Entity\User;
-use App\Entity\UserType;
 use DateTime;
-use FOS\RestBundle\Controller\Annotations as Rest;
-use FOS\RestBundle\Controller\FOSRestController;
+use Exception;
+use App\Entity\User;
+use App\Entity\Doctor;
+use App\Entity\Country;
+use App\Entity\Patient;
+use App\Entity\Hospital;
+use App\Entity\UserType;
+use App\Entity\Speciality;
 use FOS\RestBundle\View\View;
+use Vich\UploaderBundle\Entity\File;
 use JMS\Serializer\SerializerInterface;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\Serializer\Serializer;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\Validator\Constraints\Date;
-use Vich\UploaderBundle\Entity\File;
+use FOS\RestBundle\Controller\Annotations as Rest;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RestApiProfileController extends FOSRestController
 {
@@ -281,7 +282,7 @@ class RestApiProfileController extends FOSRestController
      */
     public function uploadImage(Request $request)
     {
-
+       try{
         $user = $this->getUser();
         $uploadedImage = $request->files->get('picture');
         if ($uploadedImage == null){
@@ -311,6 +312,9 @@ class RestApiProfileController extends FOSRestController
         } else {
 
             return View::create("select picture please !", JsonResponse::HTTP_BAD_REQUEST, []);
+        }
+        }catch (\Exception $e) {
+            return View::create($e->getMessage(), JsonResponse::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 }
